f8314fbb99a835ef84721c92d3e06b32d91ae72d
shimon <shimonc29@gmail.com>
2025-06-24 17:50:42 +0300

    feat(server-functions): add basic data collection handler with CORS and validation structure

--- packages/server-functions/src/functions/dataCollection.ts
+++ packages/server-functions/src/functions/dataCollection.ts
@@ -1,1 +1,20 @@
-// Data collection function implementation will be added here. 
+import { https } from 'firebase-functions';
+import { Request, Response } from 'express';
+
+export const dataCollectionHandler = https.onRequest(async (req: Request, res: Response) => {
+  // CORS headers
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).send('');
+  }
+
+  try {
+    // TODO: Validate and process events
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}); 
